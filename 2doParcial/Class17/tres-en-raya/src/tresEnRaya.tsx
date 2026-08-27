e26e37cc2813a170df5a5788015e7a0b07080864
FaaaaaAz <fAb1an12.20850@gmail.com>
2026-08-27 18:05:07 -0400

    class 18/tresEnRaya

--- 2doParcial/Class17/tres-en-raya/src/tresEnRaya.tsx
+++ 2doParcial/Class17/tres-en-raya/src/tresEnRaya.tsx
@@ -1,3 +1,18 @@
 export default function TresEnRaya() {
-    return <section>Hola</section>;
+    return (
+        <section>
+            <table>
+                <tbody>
+                    <tr>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                </tbody>
+            </table>
+        </section>
+    );
 }
